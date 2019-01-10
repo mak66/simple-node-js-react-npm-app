16d3c3cc0a26c081f300a4352bef0ed439f7290a
bert <bert@bert.com>
2019-01-10 13:30:56 +0000

    Added further tests for double function

--- src/sum.test.js
+++ src/sum.test.js
@@ -7,3 +7,6 @@ test('adds 1 + 2 to equal 3', () => {
 test('doubles 1 to equal 2', () => {
   expect(sums.double(1)).toBe(2);
 });
+test('doubles 0 to equal 0', () => {
+  expect(sums.double(0)).toBe(0);
+});
